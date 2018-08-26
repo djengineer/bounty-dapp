33995ebea35f1735c702ee070f08e1bd3d9ea446
dj <djengineer@gmail.com>
2018-08-26 11:07:16 +0800

    md docs

--- test/TestBounty.sol
+++ test/TestBounty.sol
@@ -7,6 +7,7 @@ import "../contracts/Bounty.sol";
 contract TestBounty {
   Bounty bounty = Bounty(DeployedAddresses.Bounty());
   uint public initialBalance = 1 ether;
+  // Testing Adding User
 	function testAddUser() public {
 	  bool returnedsuccess = bounty.AddUser(12,"DJ","WONG","test@test.com");
 
@@ -14,24 +15,28 @@ contract TestBounty {
 
 	  Assert.equal(returnedsuccess, expected, "User not successfully saved");
 	}
+	// Testing Adding Job
 	function testAddJob() public{
 		bool returnedsuccess = bounty.AddJob.value(0.5 ether)("Job Title","Job Desc");
 		bool expected = true;
 
 	  Assert.equal(returnedsuccess, expected, "Job not successfully added");
 	}
+	// Testing Accepting Jobs
 	function testAcceptJob() public{
 		bool returnedsuccess = bounty.AcceptJob(0);
 		bool expected = true;
 
 	  Assert.equal(returnedsuccess, expected, "Job not successfully accepted");
 	}
+	// Checking if promised balance is correct
 	function testPromisedBalance() public{
 		uint returnedsuccess = bounty.GetMyPromisedAmount();
 		uint expected = 0.5 ether;
 
 	  Assert.equal(returnedsuccess, expected, "Promised balance is not 0.5 ether");
 	}
+	// test complete job
 	function testCompleteJob() public{
 		bool returnedsuccess = bounty.CompleteJob(0);
 		bool expected = true;
@@ -59,7 +64,8 @@ contract TestBounty {
 
 	  Assert.equal(returnedsuccess, expected, "Not Successfully Toggled");
 	}
-	// should revert here
+	// should revert here because the Emergency Stop has been invoked,
+	// AddJob should not accept any transaction.
 	function testAddJobAfterStop() public{
 		bool returnedsuccess = bounty.AddJob.value(0.5 ether)("Job Title","Job Desc");
 		bool expected = false;
